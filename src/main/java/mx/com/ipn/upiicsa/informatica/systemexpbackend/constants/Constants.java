ae746da7c8736633d46284dfc8e71ef0824c8294
Erick Luna <eluna1802@alumno.ipn.mx>
2022-04-29 18:58:18 -0500

    JWT Constants has been added

---
+++ src/main/java/mx/com/ipn/upiicsa/informatica/systemexpbackend/constants/Constants.java
@@ -0,0 +1,9 @@
+package mx.com.ipn.upiicsa.informatica.systemexpbackend.constants;
+
+public class Constants {
+    public final static String SECRET = "SYS-3XP3RT";
+    public final static String USER_ID = "userId";
+    public final static String ROLE = "role";
+    public final static String AUTHORIZATION_HEADER = "Authorization";
+    public final static String BEARER_TOKEN = "Bearer ";
+}
